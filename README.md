5a8e72134773ef4692d00f6a370d0deefd59ded3
metamn <bartus.csongor@gmail.com>
2019-05-21 11:43:30 +0300

    Working on the theme

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# react-wp
+# React WP
+
 React and WordPress
+
+## Resources
+
+- https://github.com/wp-graphql/wordflix-single-page-app
+- https://github.com/postlight/headless-wp-starter
---
+++ wp-content/themes/react-wp-theme/functions.php
@@ -0,0 +1,6 @@
+<?php
+/**
+ * This file is used to do some basic setup to use Create React App as the theme, and add
+ * support for post thumbnails, etc
+ */
+require_once( __DIR__ . '/inc/setup.php' );
